00cb2c39f528ce853b0312d3a6bb8590036fb4d2
henry <88551100+henrykgggg@users.noreply.github.com>
2023-03-08 09:34:47 +0200

    combined both components

--- food-order/src/components/MEALS/Meals.js
+++ food-order/src/components/MEALS/Meals.js
@@ -1,2 +1,12 @@
-
-
+import {Fragment} from 'react'
+import MealsSummary from "./SSUMMARY.JS";
+import AvailableMeals from "./AVAILABLEMEALS.JS";
+const Meals = () => {
+    return (
+        <Fragment>
+<MealsSummary />
+<AvailableMeals />
+        </Fragment>
+    )
+}
+export default Meals;
