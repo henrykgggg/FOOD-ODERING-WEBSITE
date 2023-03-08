1697cf2a82888866a9c4eb28ca44f0614f166b25
henry <88551100+henrykgggg@users.noreply.github.com>
2023-03-08 09:37:03 +0200

    add in app.js

--- food-order/src/App.js
+++ food-order/src/App.js
@@ -1,10 +1,14 @@
 import React,{Fragment} from "react";  
 import Header from "./components/LAYOUT/Header";
+import Meals from "./components/MEALS/Meals";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 };
